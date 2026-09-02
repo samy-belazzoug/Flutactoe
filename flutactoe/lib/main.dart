98f42bedf83f1e38f6ab6dc58e6d4523df4f5cbc
samy-belazzoug <samy.belazzoug@pm.me>
2026-09-02 12:21:21 +0200

    Tile now are coloured belonging to its state and Started transition from stateless to stateful

--- flutactoe/lib/main.dart
+++ flutactoe/lib/main.dart
@@ -21,10 +21,18 @@ class Application extends StatelessWidget {
   }
 }
 
-class MainWindow extends StatelessWidget {
+class MainWindow extends StatefulWidget {
+  Game game = Game();
+  
+  @override 
+  State<MainWindow> createState() => _MainWindowState();
+}
+
+class _MainWindowState extends State<MainWindow> {
   Game game = Game();
   List<TileState> testGrid = [TileState.empty,TileState.empty,TileState.empty,TileState.cross,TileState.cross,TileState.cross,TileState.rounds,TileState.rounds,TileState.rounds,];
-  
+  String turnText = 'Play.';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,12 +45,19 @@ class MainWindow extends StatelessWidget {
       ),
       
       body: // The main content
-          gridUI(testGrid),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 50,
+            children: [
+            SizedBox(height: 80,),
+            Text(turnText, style: TextStyle(fontSize: 28),),
+            gridUI(testGrid),
+          ])
     );
   }
 }
 
-Widget? gridUI(List<TileState> grid) {
+Widget gridUI(List<TileState> grid) {
   return Column(
     mainAxisAlignment: MainAxisAlignment.center,
     crossAxisAlignment: CrossAxisAlignment.center,
@@ -55,25 +70,45 @@ Widget? gridUI(List<TileState> grid) {
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
-        child: Text(getSymbolFromTileState(grid[0]), style: TextStyle(color: Colors.white, fontSize: 76),),
+        
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[0]),
+        ),
+        
+        child:
+            GestureDetector(
+              onTap: onGridTap,
+              child: Text(getSymbolFromTileState(grid[0]), style: TextStyle(color: Colors.white, fontSize: 76),),
+            ) 
+            
       ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[1]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[1]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[2]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[2]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
     ],),
@@ -86,25 +121,40 @@ Widget? gridUI(List<TileState> grid) {
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[3]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[3]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[4]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[4]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[5]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[5]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
     ],),
@@ -117,30 +167,60 @@ Widget? gridUI(List<TileState> grid) {
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[6]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[6]), style: TextStyle(color: Colors.white, fontSize: 76),),
       ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[7]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[7]), style: TextStyle(color: Colors.white, fontSize: 76),)
         ),
       Container(
         alignment: AlignmentGeometry.center,
         margin: const EdgeInsets.all(10.0),
-        color: Colors.blueGrey,
         width: 100,
         height: 100,
+
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(7)),
+          color: updateTileColor(grid[8]),
+        ),
+
         child: Text(getSymbolFromTileState(grid[8]), style: TextStyle(color: Colors.white, fontSize: 76),),
       )
       ],
     ),
     ],);
+}
+
+Color updateTileColor(TileState state) {
+  switch (state) {
+    case TileState.cross:
+      return Colors.red;
+    case TileState.rounds:
+      return Colors.lightBlue;
+    case TileState.empty:
+      return Colors.blueGrey;
+    }
+  }
+
+void onGridTap() {
+  print("Hello, World!");
 }
 
 /*
